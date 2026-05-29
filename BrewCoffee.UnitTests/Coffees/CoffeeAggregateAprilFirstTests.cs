425ce17bf059e70d17a1c59ebf2b4b84f75219a1
k3lvindre <k3lvindre@gmail.com>
2026-05-30 00:52:54 +0800

    Add unit test to Coffee aggregate for april 1st scenario

---
+++ BrewCoffee.UnitTests/Coffees/CoffeeAggregateAprilFirstTests.cs
@@ -0,0 +1,25 @@
+using BrewCoffee.Core.Aggregates.CoffeeAggregate;
+using BrewCoffee.Core.Common.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BrewCoffee.UnitTests.Coffees
+{
+    [TestFixture]
+    public class CoffeeAggregateAprilFirstTests
+    {
+        [Test]
+        public void Brew_OnAprilFirst_ShouldThrowBusinessException()
+        {
+            // Arrange
+            var coffee = new Coffee();
+            var aprilFirst = new DateTime(DateTime.UtcNow.Year, 4, 1);
+
+            // Act
+            var act = () => coffee.Brew(aprilFirst);
+
+            // Assert
+            act.Should().Throw<BusinessException>();
+        }
+    }
+}
