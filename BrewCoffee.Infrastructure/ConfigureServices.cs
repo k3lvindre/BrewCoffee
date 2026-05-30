3fc3a735436e7790dec270ae60a83392f60ef759
k3lvindre <k3lvindre@gmail.com>
2026-05-30 10:59:39 +0800

    add weatherservice implementation in infrastructure layer

---
+++ BrewCoffee.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,18 @@
+﻿using BrewCoffee.Core.Services.Weather;
+using BrewCoffee.Infrastructure.Services.Weather;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrewCoffee.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<IWeatherService, WeatherService>();
+
+            return services;
+        }
+    }
+}
---
+++ BrewCoffee.Infrastructure/Services/Weather/WeatherService.cs
@@ -0,0 +1,26 @@
+﻿using BrewCoffee.Core.Common.Exceptions;
+using BrewCoffee.Core.Services.Weather;
+using BrewCoffee.ExternalService.Weather.OpenWeatherOrg;
+using Microsoft.Extensions.Configuration;
+
+namespace BrewCoffee.Infrastructure.Services.Weather
+{
+    public class WeatherService(IOpenWeatherOrgWeatherService openWeatherOrgWeather,
+        IConfiguration configuration) : IWeatherService
+    {
+        private readonly IOpenWeatherOrgWeatherService _openWeatherOrgWeather = openWeatherOrgWeather;
+        private readonly IConfiguration _configuration = configuration;
+
+        public async Task<int> GetCurrentWeatherTemparatureAsync()
+        {
+            var currentWeatherData = await _openWeatherOrgWeather.GetCurrentWeatherDataAsync(52.2297, 
+                21.0122,
+                _configuration["ExternalService:Weather:OpenWeatherMapOrg:ApiKey"]!);
+
+            var currentTemparature = currentWeatherData?.Content?.Data.FirstOrDefault()?.Temperature ?? 
+                throw new BusinessException();
+
+            return Convert.ToInt32(currentTemparature);
+        }
+    }
+}
