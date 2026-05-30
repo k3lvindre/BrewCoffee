19180972a096fda419b51dd62f064d78c982f14b
k3lvindre <k3lvindre@gmail.com>
2026-05-30 11:30:48 +0800

    Fix url error with IOpenWeatherOrgWeatherService

--- BrewCoffee.ExternalService/Weather/OpenWeatherOrg/IOpenWeatherOrgWeatherService.cs
+++ BrewCoffee.ExternalService/Weather/OpenWeatherOrg/IOpenWeatherOrgWeatherService.cs
@@ -4,7 +4,7 @@ namespace BrewCoffee.ExternalService.Weather.OpenWeatherOrg
 {
     public interface IOpenWeatherOrgWeatherService
     {
-        [Get("?lat={lat}&lon={lon}&units=metric&appid={apikey}")]
+        [Get("/current?lat={lat}&lon={lon}&units=metric&appid={apikey}")]
         Task<ApiResponse<OpenWeatherMapOrgGetCurrentWeatherDataResponse>> GetCurrentWeatherDataAsync([AliasAs("lat")] double latitude,
             [AliasAs("lon")] double longtitude, 
             [AliasAs("apikey")] string apiKey);
