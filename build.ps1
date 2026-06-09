2d4ba5939024add26d606c1b4c669c22bad31b19
k3lvindre <k3lvindre@gmail.com>
2026-06-10 06:35:45 +0800

    add release pipeline yaml

--- build.ps1
+++ build.ps1
@@ -40,6 +40,17 @@ Function UnitTests{
 	}
 }
 
+# For future use, we can automate the database migration or setup here.
+# especially for setting up new dev env or for new developer
+Function MigrateDatabaseLocal {
+	Write-Host "Starting LocalDB"
+	sqllocaldb start mssqllocaldb
+
+	exec{
+		& $aliaSql $databaseAction $databaseServer $databaseName $databaseScripts
+	}
+}
+
 Function PrivateBuild{
 	$sw = [Diagnostics.Stopwatch]::StartNew()
 	Init
